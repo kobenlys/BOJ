3c4922425d792488e62727e58d659443d970e8f2
Koban <149328708+DevKoban@users.noreply.github.com>
2023-11-24 05:20:43 +0900

    Update 두 용액.java

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -8,7 +8,7 @@ public class Main {
     public static int min = Integer.MAX_VALUE, len;
 
 
-    public static void algorithm(){
+    public static void algorithm(){ // 투포인터 알고리즘 구현
         int sum=0, left=0, right= len-1;
         String res = "";
 
@@ -16,18 +16,18 @@ public class Main {
 
             sum = arr1[left] + arr1[right];
 
-            if(min > Math.abs(sum)){
+            if(min > Math.abs(sum)){ // min 과 절댓값 비교 = -1, 1은 0과 거리는 같음
                 min = Math.abs(sum);
                 res = arr1[left]+" "+arr1[right];
             }
 
-            if (sum > 0) {
+            if (sum > 0) { //sum이 크다면 오름차순이기때문에 right--;
                 right--;
-            } else if (sum < 0) {
+            } else if (sum < 0) { // sum이 작다면 오름차순이기때문에 left++;
                 left++;
             }
 
-            if (sum == 0) {
+            if (sum == 0) { // 0과 같다면 res 저장 후 while문 break
                 res = arr1[left]+" "+arr1[right];
                 break;
             }
@@ -46,9 +46,9 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < len; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken()); // ok
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr1);
+        Arrays.sort(arr1); // 오름차순 정렬
         algorithm();
 
     }
