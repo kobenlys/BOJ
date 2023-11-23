b802168096e919b50d0d1415e190a49e9a24aa4f
Koban <149328708+DevKoban@users.noreply.github.com>
2023-11-24 05:36:56 +0900

    평범한 배낭.java
    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51104 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,24 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[][] item;
+   public static int N, K;
+   public static int[][] item;
     public static int[][] dp;
-    public static int N, K;
 
     public static void algorithm() {
 
-        for (int i = 1; i <= N ; i++) {
+        for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= K; j++) {
-                if (j < item[i][0]) {
+                if (item[i][0] > j) { // J = 현재 무게 보다 물건의 무게가 더 크다면 윗 행 복사
                     dp[i][j] = dp[i - 1][j];
-                }else{
+                }else{ // 현재 무게 보다 물건의 무게가 적거나 같다면 더 큰 값으로 갱신
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - item[i][0]] + item[i][1]);
                 }
             }
         }
-
     }
-
 
     public static void main(String[] args) throws IOException { //조건 입력
 
@@ -30,21 +28,20 @@ public class Main {
 
         st = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(st.nextToken()); // 물품 수
+        N = Integer.parseInt(st.nextToken()); // 물건 수
         K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
 
-        item = new int[N+1][2];
+        item = new int[N +1][2];
         dp = new int[N + 1][K + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-
             item[i][0] = Integer.parseInt(st.nextToken()); // 물건 무게
-            item[i][1] = Integer.parseInt(st.nextToken()); // 물건 밸류
+            item[i][1] = Integer.parseInt(st.nextToken()); // 물건 만족도
         }
 
         algorithm();
+        //결국 테이블에서 가장 끝부분이 최적값임.
         System.out.println(dp[N][K]);
     }
-
 }
