86aedc1015860c1e8d8f58d7a3a5eddf38393536
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-10 05:38:41 +0900

    [Silver I] Title: 물병, Time: 92 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.java
+++ 백준/Silver/1052. 물병/물병.java
@@ -13,23 +13,8 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
         int ans = 0;
 
-        // 문제 "같은 양 물병 2개씩 합칠 수 있음" -> 2진수 패턴
-        // 비트마스킹 풀이 가능하다
-        
-        // ans++ 해주면서 이진수의 '1'의 수를 카운팅하고
-        // K이하 라면 break 후 출력한다
-        // 답이 나오기 전까지 무한반복한다.
-        while (true) {
-            int cnt = 0;
-            String bitN = Integer.toBinaryString(N+ans);
-            for (int i = 0; i < bitN.length(); i++) {
-                if (bitN.charAt(i) == '1') {
-                    cnt++;
-                }
-            }
-            if (cnt <= K) {
-                break;
-            }
+        while (Integer.bitCount(N) > K) {
+            N++;
             ans++;
         }
 
@@ -37,3 +22,5 @@ public class Main {
         System.out.println(ans);
     }
 }
+
+
