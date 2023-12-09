58c5eec0986c92a27175b6f5456d7aead9aef3a3
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-10 02:15:29 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 29440 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
@@ -12,22 +11,18 @@ public class Main {
         Stack<Integer> stk = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
-        String str = "";
-        String res = "";
 
         // 초기 설정
         stk.push(1); // 1부터 시작 미리 넣는다.
         int flag = 2;   // 다음 스택 입력 수
-        int temp = 0;
         sb.append("+").append("\n");
-
 
         st:
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
 
             while (!stk.isEmpty()) {
-                if (stk.peek() == num) {
+                if (stk.peek() == num) { // 같다면 pop
                     stk.pop();
                     sb.append("-").append("\n");
                     if (stk.isEmpty()) {
@@ -35,10 +30,12 @@ public class Main {
                         sb.append("+").append("\n");
                     }
                     break;
-                } else if (stk.peek() < num) {
+                } else if (stk.peek() < num) { // num 보다 peek가 작다면 push
                     stk.push(flag++);
                     sb.append("+").append("\n");
                 } else if (stk.peek() > num) {
+                    // 스택 수열 은 stk.peek 가 num 보다 클 수 없음
+                    // 스택 수열의 규칙임.
                     System.out.println("NO");
                     System.exit(0);
                 }
@@ -46,8 +43,6 @@ public class Main {
 
         }
 
-        System.out.print(sb.deleteCharAt(sb.length()-2));
+        System.out.print(sb.deleteCharAt(sb.length() - 2));
     }
 }
-
-
