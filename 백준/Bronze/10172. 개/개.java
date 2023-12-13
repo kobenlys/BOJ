2e3b893aca68a6818f8485e22f20e588fb801f83
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-13 18:03:49 +0900

    [Bronze V] Title: 개, Time: 68 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException { // 값 입력
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|").append("\n").append("|q p|   /}")
+                .append("\n").append("( 0 )\"\"\"\\").append("\n").append("|\"^\"`    |")
+                .append("\n").append("||_/=\\\\__|");
+        System.out.println(sb);
+    }
+}
+
+
