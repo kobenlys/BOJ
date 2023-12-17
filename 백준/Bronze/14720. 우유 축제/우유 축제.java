3752c2f2c1510e7a0097be6a7d19e4ace601bc07
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-17 21:42:14 +0900

    [Bronze III] Title: 우유 축제, Time: 84 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.java
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -9,16 +9,21 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
+        // 우유 마시는 규칙 저장
         int[] arr1 = {0, 1, 2};
         int cnt = 0;
         int milk = 0;
         st = new StringTokenizer(br.readLine(), " ");
-
+        
+        // st에 요소가 존재한다면 반복
         while (st.hasMoreElements()) {
             int store = Integer.parseInt(st.nextToken());
+            // arr1 배열 이용하여 우유 마시는 규칙 순서 정함
             if (store == arr1[cnt]) {
+                // 다음 우유 먹기 위해 cnt++
                 cnt++;
                 milk++;
+                // 다시 딸기우유를 먹어야 하기 위해 cnt 초기화
                 if (cnt == 3) {
                     cnt = 0;
                 }
