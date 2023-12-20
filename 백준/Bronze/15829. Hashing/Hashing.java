6929a6a35445fc5bd814858e6d9886ee31b617f5
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-20 23:35:36 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 11744 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
-        int ans = 0;
+        BigInteger ans = new BigInteger("0");
+        BigInteger r = new BigInteger("31");
         String input = br.readLine();
 
         for (int i = 0; i < L; i++) {
-            ans += (int) ((input.charAt(i) - 96) * Math.pow(31, i)) % 1234567891;
+            BigInteger str = BigInteger.valueOf(input.charAt(i) - 'a' + 1);
+            BigInteger numP = r.pow(i);
+            ans = ans.add(str.multiply(numP).mod(BigInteger.valueOf(1234567891)));
         }
-        System.out.print(ans);
+        System.out.print(ans.mod(BigInteger.valueOf(1234567891)));
     }
 }
