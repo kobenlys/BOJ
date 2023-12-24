7c266a6505863c6fa17c6b80a8b94c1a9a05517c
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-24 17:46:59 +0900

    [Silver I] Title: Z, Time: 88 ms, Memory: 12976 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -4,30 +4,28 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int N, r, c, cnt;
+    public static int N, Y, X, cnt;
 
+    public static void algorithm(int x, int y, int size) {
 
-    public static void algorithm(int size, int x, int y) {
         if (size == 2) {
             calc(x, y);
             return;
         }
 
         int nSize = size / 2;
-        // 분할정복 + 재귀만 이용하면 시간초과가 난다
-        // 그렇다면 1~4분면 을 다 탐색 하지 않고 8*8 배열의 중간값 4,4를 기준으로 1~4분면을 나눠야 한다.
-        // 즉 이진탐색기법을 사용해서 배열의 사이즈마다 중간 값을 구하고 r,c 가 어떤 분면에 속하는지에 따라 재귀한다.
-        if (r < y + nSize && c < x + nSize) { // 제 2사분면, 왼쪽 위
-            algorithm(nSize, x, y);
-        } else if (r < y + nSize && c >= x + nSize) { //제 1사분면, 오른쪽 위
+
+        if (X < x + nSize && Y < y + nSize) {
+            algorithm(x, y, nSize);
+        } else if (X >= x + nSize && Y < y + nSize) {
             cnt += nSize * nSize;
-            algorithm(nSize, x + nSize, y);
-        } else if (r >= y + nSize && c < x + nSize) { //제 3사분면, 왼쪽 아래
+            algorithm(x + nSize, y, nSize);
+        } else if (X < x + nSize && Y >= y + nSize) {
             cnt += nSize * nSize * 2;
-            algorithm(nSize, x, y + nSize);
-        } else {//제 4사분면, 오른쪽 아래
+            algorithm(x, y + nSize, nSize);
+        } else {
             cnt += nSize * nSize * 3;
-            algorithm(nSize, x + nSize, y + nSize);
+            algorithm(x + nSize, y + nSize, nSize);
         }
     }
 
@@ -35,11 +33,12 @@ public class Main {
         int[] dx = {0, 1, 0, 1};
         int[] dy = {0, 0, 1, 1};
 
+
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx == c && ny == r) {
-                // Z로 움직인 만큼 i를 더해준다
+
+            if (nx == X && ny == Y) {
                 System.out.println(cnt + i);
                 System.exit(0);
             }
@@ -51,10 +50,10 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        int size = (int) Math.pow(2, N);
+        Y = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        algorithm(size, 0, 0);
+        int newSize = (int) Math.pow(2, N);
+        algorithm(0, 0, newSize);
     }
 }
