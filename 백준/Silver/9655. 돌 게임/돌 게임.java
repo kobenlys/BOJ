245348247bd71b94e3c744444abfebd5bf853ee2
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-26 00:47:31 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,18 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-
-        if (N % 2 == 0) {
+        // 이진수로 변환
+        String str = Integer.toBinaryString(Integer.parseInt(br.readLine()));
+        
+        // 이진수에서 가장 오른쪽 숫자가 "1"이라면 홀수이다.
+        if (str.charAt(str.length()-1) == '0') {
             System.out.println("CY");
         } else {
             System.out.println("SK");
