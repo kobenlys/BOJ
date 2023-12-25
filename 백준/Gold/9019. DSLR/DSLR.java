2f911f094929ab1f3ad72e5467a7003e7c3c4278
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-26 05:37:32 +0900

    [Gold IV] Title: DSLR, Time: 2984 ms, Memory: 296216 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -4,72 +4,62 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int goal;
-    public static Queue<node> qu = new LinkedList<>();
-    public static StringBuilder sb = new StringBuilder();
+    public static int start, goal;
+    public static char[] flag = {'D', 'S', 'L', 'R'};
     public static boolean[] vi;
+    public static Queue<Integer> qu = new LinkedList<>();
+    public static StringBuilder sb = new StringBuilder();
+    public static StringBuilder res = new StringBuilder();
 
-    public static class node { // 계산된 값과 어떤계산을 진행했는지 기록하는 객체
-        int res;
-        String trace;
 
-        public node(int res, String trace) {
-            this.res = res;
-            this.trace = trace;
+    public static int instDSLR(int i, int num) {
+        if (i == 0) { // D 계산
+            return (num * 2) % 10000;
+        } else if (i == 1) { // S 계산
+            return num == 0 ? 9999 : num - 1;
+        } else if (i == 2) {
+            // L 계산 , 자릿수 왼쪽 회전
+            return num % 1000 * 10 + num / 1000;
+        } else {
+            // R 계산 , 자릿수 오른쪽 회전
+            return num % 10 * 1000 + num / 10;
         }
     }
 
-    public static int D(int num) { // D 계산, 숫자 두배로 변환
-        int temp = num * 2;
-        return temp % 10000;
-    }
-
-    public static int S(int num) { // S 계산, 숫자 -1 하기
-        int temp = num - 1;
-        // temp 가 -1 일경우 9999를 리턴한다
-        return temp == -1 ? 9999 : temp;
-    }
-
-    public static int L(int num) { // L 계산 , 자릿수 왼쪽 회전
-        return num % 1000 * 10 + num / 1000;
-    }
-
-    public static int R(int num) { // R 계산 , 자릿수 오른쪽 회전
-        return num % 10 * 1000 + num / 10;
-    }
 
     public static void bfs(int start) { // BFS 알고리즘
-        qu.offer(new node(start, ""));
+        int[] from = new int[10000];
+        char[] Calc = new char[10000];
+        qu.offer(start);
         vi[start] = true;
-        while (!qu.isEmpty()) {
-            node nd = qu.poll();
 
-            if (nd.res == goal) { // 답 출력
-                sb.append(nd.trace).append("\n");
+        while (!qu.isEmpty()) {
+            int key = qu.poll();
+
+            if (key == goal) { // 답 출력
+                // 역 추적하기.
+                int tmp = goal;
+                while (start != tmp) {
+                    // 가장 마지막에 나오는 답이 첫번째 명령어이기 때문에 insert 메서드 사용하여 첫번째 자리에 입력
+                    sb.insert(0, Calc[tmp]);
+                    tmp = from[tmp];
+                }
+                res.append(sb).append("\n");
+                sb.setLength(0);
                 qu.clear();
                 break;
             }
 
-            int d = D(nd.res);
-            int s = S(nd.res);
-            int l = L(nd.res);
-            int r = R(nd.res);
-            // 이미 나온 결과값은 다시 계산할 필요없다
-            if (!vi[d]) {
-                qu.offer(new node(d, nd.trace + "D"));
-                vi[d] = true;
-            }
-            if (!vi[s]) {
-                qu.offer(new node(s, nd.trace + "S"));
-                vi[s] = true;
-            }
-            if (!vi[l]) {
-                qu.offer(new node(l, nd.trace + "L"));
-                vi[l] = true;
-            }
-            if (!vi[r]) {
-                qu.offer(new node(r, nd.trace + "R"));
-                vi[r] = true;
+            for (int i = 0; i < 4; i++) {
+                int num = instDSLR(i, key);
+
+                if (!vi[num]) { // 이미 나왔던 결과값은 다시 계산할 필요 없다.
+                    qu.offer(num);
+                    // 역추적 위한 현재 계산된 값에 대응하는 배열자리에 전 값 저장
+                    from[num] = key;
+                    Calc[num] = flag[i]; // 현재 값에 대응하는 자리에 명령어 입력
+                    vi[num] = true;
+                }
             }
         }
     }
@@ -82,11 +72,11 @@ public class Main {
 
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
+            start = Integer.parseInt(st.nextToken());
             goal = Integer.parseInt(st.nextToken());
             vi = new boolean[10000];
             bfs(start);
         }
-        System.out.print(sb);
+        System.out.print(res);
     }
 }
