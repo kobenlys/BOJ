141767d51844dd9e5ab89054aa73af881c889b02
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-28 19:21:21 +0900

    [Silver II] Title: 좌표 압축, Time: 1812 ms, Memory: 359732 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -6,31 +6,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, Integer> map = new HashMap<>(); // HashMap 을 이용한 좌표압축
+        HashMap<Integer, Integer> map = new HashMap<>();
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         int[] arr1 = new int[N];
-        int[] copy;
+        int[] copy = new int[N];
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+            // 값 압축 후 원래 배열 순서대로 출력할때 필요하기때문에 복사배열 하나 만든다.
+            copy[i] = arr1[i] = Integer.parseInt(st.nextToken());
         }
-
-        copy = Arrays.copyOf(arr1, N);
-        Arrays.sort(copy); // copy배열을 정렬하여 최솟값부터 HashMap에 입력 및 순위 기록
-
+        // 순서 매길때 필요하기때문에 정렬한다.
+        Arrays.sort(copy);
         int cnt = 0;
-        for (int e : copy) {
-            // 중복된 값은 스킵한다
-            if (!map.containsKey(e)) {
-                map.put(e, cnt);
-                cnt++;
+        for (int i = 0; i < N; i++) {
+
+            int k = copy[i];
+            // hashMap 을 이용한 압축 알고리즘
+            if (!map.containsKey(k)) {
+                map.put(k, cnt++);
             }
         }
-        // 원래 배열을 통해 순서에 맞게 순위를 출력한다.
+
         for (int i = 0; i < N; i++) {
             sb.append(map.get(arr1[i])).append(" ");
         }
