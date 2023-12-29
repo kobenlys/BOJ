3bcfcc676e31db70f76be643b6d4463e524d837e
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-29 22:07:27 +0900

    [Bronze III] Title: 세 수, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int[] arr1 = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr1);
+        System.out.print(arr1[1]);
+    }
+}
