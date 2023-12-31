531269e6546df613bf1aab58bcaff044e6071f21
영석 <149328708+DevKoben@users.noreply.github.com>
2023-12-31 20:37:27 +0900

    [Gold V] Title: AC, Time: 800 ms, Memory: 98920 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -6,10 +6,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> dq = new ArrayDeque<>(); // deque 이용해야한다.
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-
+        
         int T = Integer.parseInt(br.readLine());
 
         for (int tcase = 0; tcase < T; tcase++) {
@@ -17,6 +17,7 @@ public class Main {
             String input = br.readLine();
 
             int N = Integer.parseInt(br.readLine());
+            // 숫자만 입력받기 위해
             String str = br.readLine();
             str = str.replace("[", "");
             str = str.replace("]", "");
@@ -26,28 +27,31 @@ public class Main {
                 dq.add(Integer.parseInt(st.nextToken()));
             }
 
-            boolean check = false;
-            boolean isOk = false;
+            boolean check = false; // 배열을 실제로 뒤집을 필요는 없다, 시간초과남
+            boolean isOk = false; // 에러 판단
             for (int i = 0; i < input.length(); i++) {
                 if (input.charAt(i) == 'R') {
                     check = !check;
                 } else {
-                    if (dq.isEmpty()) {
+                    if (dq.isEmpty()) { // 에러 출력
                         sb.append("error").append("\n");
                         isOk = true;
                         break;
                     }
-                    if (!check) {
+                    if (!check) { // 배열 순서에 따른 요소 삭제
+                        // 정방향 경우
                         dq.removeFirst();
                     } else {
+                        // 역방향 경우
                         dq.removeLast();
                     }
                 }
             }
 
-            if (!isOk) {
+            if (!isOk) { // 에러가 안났다면
                 int len = dq.size();
                 if (!check) {
+                    // 정방향 출력
                     sb.append("[");
                     for (int i = 0; i < len; i++) {
                         sb.append(dq.pollFirst());
@@ -57,17 +61,18 @@ public class Main {
                     }
                     sb.append("]").append("\n");
                 } else {
+                    // 역방향 출력
                     sb.append("[");
-                    for (int i = len - 1; i >= 0; i--) {
+                    for (int i = 0; i < len; i++) {
                         sb.append(dq.pollLast());
-                        if (i != 0) {
+                        if (i != len - 1) {
                             sb.append(",");
                         }
                     }
                     sb.append("]").append("\n");
                 }
             }
-            dq.clear();
+            dq.clear(); // 초기화
         }
         System.out.print(sb);
     }
