20014101168ef721a24694f6334e77a66399cfa9
영석 <149328708+DevKoben@users.noreply.github.com>
2024-01-06 22:14:24 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 468 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
@@ -10,14 +9,17 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int cnt = 0;
+        int flag = 10;
+        int num = 1;
 
         for (int i = 1; i <= N; i++) {
-            int num = i;
-            while (num != 0) {
-                num = num / 10;
-                cnt++;
+            if (i % flag == 0) {
+                num++;
+                flag *= 10;
             }
+            cnt += num;
         }
+
         System.out.print(cnt);
     }
 }
