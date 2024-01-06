0ff814589dfb4cce069896155df91c2e79f14e6d
영석 <149328708+DevKoben@users.noreply.github.com>
2024-01-06 23:00:41 +0900

    [Silver II] Title: 한 줄로 서기, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -4,63 +4,28 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int[] arr1;
-    public static boolean[] vi;
-    public static ArrayList<Integer> res = new ArrayList<>();
-
-    public static void dfs(int start) { // DFS 알고리즘
-
-        if (start == N) { // 정답 확인
-            boolean isSort = true;
-
-            for (int i = 0; i < res.size(); i++) {
-                int cnt = 0;
-                for (int j = 0; j < i; j++) {
-                    // i 번째 자리의 숫자가 왼쪽에 있는 숫자보다 클때 카운팅
-                    if (res.get(i) < res.get(j)) {
-                        cnt++;
-                    }
-                }
-                // 문제에서 입력한 조건과 부합하는지 판단
-                if (arr1[res.get(i)-1] != cnt) {
-                    isSort = false;
-                    break;
-                }
-            }
-            if (isSort) {
-                for (int e : res) {
-                    System.out.print(e+" ");
-                }
-                System.exit(0);
-            }
-            return;
-        }
-            
-        for (int i = 1; i <= N; i++) {
-            if (!vi[i]) { // 방문안된 숫자 방문
-                vi[i] = true; // 방문 처리
-                res.add(i);
-                dfs(start+1);
-                res.remove(res.size() - 1);
-                vi[i] = false;
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException { //조건 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        arr1 = new int[N];
-        vi = new boolean[N+1];
-
+        int N = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[N+1];
+        // 리스트 자료형의 성질 i번째 자리에 넣으면 원래 i번 자리의 원소는 뒤로 밀려나고, 저장됨
+        // 이용한다.
+        ArrayList<Integer> res = new ArrayList<>();
+        
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr1[i] = Integer.parseInt(st.nextToken());
         }
-        // 함수 실행
-        dfs(0);
+
+        for (int i = N; i >0 ; i--) {
+            res.add(arr1[i],i);
+        }
+
+        for (int e : res) {
+            System.out.print(e+" ");
+        }
     }
 }
