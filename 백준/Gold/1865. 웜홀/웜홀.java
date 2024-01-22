7517b3830bdb9fe989bd520675b82d95850b3f4b
영석 <149328708+DevKoben@users.noreply.github.com>
2024-01-23 02:38:50 +0900

    [Gold III] Title: 웜홀, Time: 360 ms, Memory: 35620 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -4,43 +4,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int N, M, W, INF = Integer.MAX_VALUE;
-    public static ArrayList<ArrayList<node>> arr1;
+    public static int N, M, W;
     public static int[] dist;
+    public static ArrayList<ArrayList<node>> arr1;
 
-
-    public static class node implements Comparable<node> {
+    public static class node implements Comparable<node> { // 간선을 담는 객체
         int goal, time;
 
         public node(int goal, int time) {
             this.goal = goal;
             this.time = time;
         }
-
         @Override
         public int compareTo(node o) {
             return goal - o.goal;
         }
     }
 
-    public static boolean dijkstra(int start) {
+    public static boolean bellmanFord() { // 벨만-포드 알고리즘
         boolean isNegative = false;
 
         for (int i = 0; i < N; i++) {
             isNegative = false;
-
             for (int j = 0; j < N; j++) {
                 for (node nd : arr1.get(j)) {
-
+                    // 저장된 dist 값보다 더 작은값, 즉 음의 사이클이 있다면
                     if (dist[nd.goal] > dist[j] + nd.time) {
+                        // 초기화
                         dist[nd.goal] = dist[j] + nd.time;
                         isNegative = true;
                         if (i == N - 1) {
+                            // 마지막 노드까지 계속 업데이트가 되었다면
+                            // 이 그래프는 음의 사이클이 존재한다
                             return true;
                         }
                     }
                 }
             }
+            // 도중에 업데이트 되지 않는 노드가 있다면
+            // 음의 사이클은 존재하지 않는다.
             if (!isNegative) {
                 break;
             }
@@ -50,16 +52,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException { //조건 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-
         int T = Integer.parseInt(br.readLine());
 
         for (int tc = 0; tc < T; tc++) {
+
             st = new StringTokenizer(br.readLine(), " ");
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
 
+            // ArrayList를 사용한 인접리스트. 간선을 표현한다.
             dist = new int[N];
             arr1 = new ArrayList<>();
 
@@ -74,13 +78,17 @@ public class Main {
                 int t = Integer.parseInt(st.nextToken());
 
                 if (i < M) {
+                    // 간선 표시
                     arr1.get(s).add(new node(e, t));
                     arr1.get(e).add(new node(s, t));
                 } else {
+                    // 웜홀 , 음수 간선 표시
                     arr1.get(s).add(new node(e, -t));
                 }
             }
-            System.out.println(dijkstra(0) ? "YES" : "NO");
+            // 알고리즘 호출 + 출력, 삼항연산자 이용
+            sb.append(bellmanFord() ? "YES" : "NO").append("\n");
         }
+        System.out.print(sb);
     }
 }
