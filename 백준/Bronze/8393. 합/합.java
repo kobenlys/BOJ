10086038219343e5905d1e363674f14d33613cdf
영석 <149328708+DevKoben@users.noreply.github.com>
2024-01-26 20:09:27 +0900

    [Bronze V] Title: 합, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException { //조건 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 1; i <= N ; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
