f0d6acd086449b900ff2ebf1ccad2187ab11c38c
영석 <149328708+DevKoben@users.noreply.github.com>
2024-01-31 01:58:36 +0900

    [Bronze V] Title: Rook, Time: 72 ms, Memory: 11380 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException { //조건 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+
+    }
+}
