c9586b46b069035a0fe07302968732d5cd08d2b7
영석 <149328708+DevKoben@users.noreply.github.com>
2024-02-01 01:22:27 +0900

    [Gold II] Title: 보석 도둑, Time: 1792 ms, Memory: 129612 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -5,31 +5,29 @@ import java.util.*;
 
 public class Main {
     public static int N, K;
-    public static node[] arr1;
     public static int[] bags;
+    public static node[] arr1;
 
-    public static class node{
-        int weight, value;
-
-        public node(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
+    public static class node {
+        int w, v;
+        public node(int w, int v) {
+            this.w = w;
+            this.v = v;
         }
     }
 
     public static void greedy() {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         long ans = 0;
-        int j =0;
+        int j = 0;
         for (int i = 0; i < K; i++) {
             while (j < N) {
-                if (arr1[j].weight <= bags[i]) {
-                    pq.offer(arr1[j++].value);
+                if (arr1[j].w <= bags[i]) {
+                    pq.offer(arr1[j++].v);
                 } else {
                     break;
                 }
             }
-
             if (!pq.isEmpty()) {
                 ans += pq.poll();
             }
@@ -45,24 +43,24 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
         arr1 = new node[N];
         bags = new int[K];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             arr1[i] = new node(w, v);
         }
-        Arrays.sort(arr1, (g1, g2) -> {
-            if(g1.weight == g2.weight) return g2.value - g1.value;
-            return g1.weight - g2.weight;
-        });
 
         for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            bags[i] = Integer.parseInt(st.nextToken());
+            bags[i] = Integer.parseInt(br.readLine());
         }
 
-
+        Arrays.sort(arr1, (g1, g2) -> {
+            if(g1.w == g2.w) return g2.v - g1.v;
+            return g1.w - g2.w;
+        });
         Arrays.sort(bags);
+
         greedy();
     }
 }
