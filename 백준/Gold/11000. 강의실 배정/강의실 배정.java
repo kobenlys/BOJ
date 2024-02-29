81e16c9715754ab286cf451bc493fa6768155470
영석 <149328708+DevKoben@users.noreply.github.com>
2024-02-29 15:51:21 +0900

    [Gold V] Title: 강의실 배정, Time: 612 ms, Memory: 76320 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -17,15 +17,22 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            startQ.offer(s);
-            endQ.offer(e);
+            startQ.offer(s); // 내림차순 시작시간 담는 우선순위 큐
+            endQ.offer(e); // 내림차순 끝나는 시간 담는 우선순위 큐
         }
+
         int cnt = 0;
         while (!startQ.isEmpty() && !endQ.isEmpty()) {
             if (startQ.peek() < endQ.peek()) {
+                // 시작시간 보다 끝나는 시간이 큰 경우
+                // 즉 강의실 하나 필요함.
                 cnt++;
                 startQ.poll();
             } else {
+                // 시간 시작시간과 끝나는 시간이 같은 경우 = 강의실 재사용
+                // ex) 1시 ~ "3시", "3시"(시작시간) ~ 5시 = 강의실 1개면 충분.
+                // 시작시간이 끝나는 시간보다 큰 경우 = 강의실 재사용
+                // ex) 1시 ~ "3시", "7시"(시작시간) ~ 10시
                 startQ.poll();
                 endQ.poll();
             }
