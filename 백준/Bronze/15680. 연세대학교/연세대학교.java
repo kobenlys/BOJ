babed207a917e16f238f0080e64e949f4d958184
영석 <149328708+DevKoben@users.noreply.github.com>
2024-03-02 21:21:25 +0900

    [Bronze V] Title: 연세대학교, Time: 72 ms, Memory: 11312 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException { //조건 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N==0?"YONSEI":"Leading the Way to the Future");
+        
+    }
+}
