db12a1a7b087b5f45afcaacd17e2664e7f884664
영석 <149328708+DevKoben@users.noreply.github.com>
2024-03-03 20:29:23 +0900

    [Silver I] Title: 트리 순회, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -4,7 +4,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static node[] tree;
+    public static node[] arr1;
+    public static StringBuilder sb = new StringBuilder();
+
     public static class node {
         char val;
         node left, right;
@@ -17,60 +19,61 @@ public class Main {
     }
 
     public static void preorder(node nd) {
-        if(nd == null) return;
-        System.out.print(nd.val);
+        if (nd == null) return;
+        sb.append(nd.val);
         preorder(nd.left);
         preorder(nd.right);
     }
 
     public static void inorder(node nd) {
-        if(nd == null) return;
+        if (nd == null) return;
         inorder(nd.left);
-        System.out.print(nd.val);
+        sb.append(nd.val);
         inorder(nd.right);
     }
 
     public static void postorder(node nd) {
-        if(nd == null) return;
+        if (nd == null) return;
         postorder(nd.left);
         postorder(nd.right);
-        System.out.print(nd.val);
+        sb.append(nd.val);
     }
-
 
     public static void main(String[] args) throws IOException { //조건 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        tree = new node[N];
+        arr1 = new node[N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-
             char parent = st.nextToken().charAt(0);
             char left = st.nextToken().charAt(0);
             char right = st.nextToken().charAt(0);
 
-            if (tree[parent - 'A'] == null) { // 노드 생성
-                tree[parent - 'A'] = new node(parent);
+            if (arr1[parent - 'A'] == null) {
+                arr1[parent - 'A'] = new node(parent);
             }
-            if (left != '.') { // 부모노드에 left노드 연결
-                tree[left - 'A'] = new node(left);
-                tree[parent - 'A'].left = tree[left - 'A'];
+
+            if (left != '.') {
+                // arr1에 left 노드를 생성하고
+                // arr1에 부모노드.왼쪽에 방금 생선한 left노드를 연결한다.
+                arr1[left - 'A'] = new node(left);
+                arr1[parent - 'A'].left = arr1[left - 'A'];
             }
-            if (right != '.') { // 부모노드에 right노드 연결
-                tree[right - 'A'] = new node(right);
-                tree[parent - 'A'].right = tree[right - 'A'];
+            if (right != '.') {
+                arr1[right - 'A'] = new node(right);
+                arr1[parent - 'A'].right = arr1[right - 'A'];
             }
         }
-        // 전위 순회 = root -> left -> right
-        preorder(tree[0]);
-        System.out.println();
-        // 중위 순회 = left -> root -> right
-        inorder(tree[0]);
-        System.out.println();
-        // 후위 순회 = left -> right -> root
-        postorder(tree[0]);
+
+        preorder(arr1[0]);
+        sb.append("\n");
+        inorder(arr1[0]);
+        sb.append("\n");
+        postorder(arr1[0]);
+
+        System.out.print(sb);
     }
 }
