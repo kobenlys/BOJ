fa65a7346b2261070656b384eb13de86450cf651
영석 <149328708+DevKoben@users.noreply.github.com>
2024-03-25 17:08:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 560 ms, Memory: 49612 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -14,29 +14,23 @@ public class Main {
             this.v = v;
         }
 
-        @Override // 최소값부터 계산하여 사이클 생긴다면 제외 할거임.
+        @Override
         public int compareTo(node o) {
             return v - o.v;
         }
     }
-    // 부모 확인
+
     public static int find(int x) {
         if(parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
-    // 같은 집합 (부모 연결) 해주기 -> parent 배열에 부모를 밸류로 설정
+
     public static void union(int x, int y) {
         x = find(x);
         y = find(y);
         if (x != y) {
             parent[y] = x;
         }
-    }
-    // 부모가 같은지 판단. -> 부모가 같다면 사이클 발생 -> 트리 아님.
-    public static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
-        return x == y;
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,7 +40,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        // 초기 값 설정.
+
         parent = new int[V];
         for (int i = 0; i < V; i++) {
             parent[i] = i;
@@ -65,11 +59,11 @@ public class Main {
         for (int i = 0; i < E; i++) {
 
             node nd = pq.poll();
-            int from = find(nd.s); // 부모노드가 있다면 부모노드 가져오기
+            int from = find(nd.s);
             int to = find(nd.e);
-            // 트리 조건(사이클 존재 x)에 맞는지 확인
-            // 부모가 같다면 -> 사이클 존재 한다는 뜻.
-            if (!isSameParent(from, to)) {
+            
+            if (from != to) {
+                
                 total += nd.v;
                 union(nd.s, nd.e);
             }
