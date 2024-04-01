43d76cab08195bdaa5f3aacf35d103da0c412ea8
영석 <149328708+DevKoben@users.noreply.github.com>
2024-04-01 16:21:46 +0900

    [Gold I] Title: 열쇠, Time: 612 ms, Memory: 168676 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -6,12 +6,10 @@ import java.util.*;
 public class Main {
     public static int Y, X, cnt;
     public static char[][] arr1;
-    public static HashMap<Character, node> map;
     public static HashSet<Character> key;
 
     public static class node {
         int x, y;
-
         public node(int x, int y) {
             this.x = x;
             this.y = y;
@@ -35,10 +33,11 @@ public class Main {
                 if (nx < 0 || ny < 0 || nx >= X + 2 || ny >= Y + 2) continue;
 
                 char k = arr1[ny][nx];
-
+                // 열쇠를 발견시
                 if (k != '.' && Character.isLowerCase(k)) {
+                    // key에 해당 키 입력 후 방문배열 초기화. -> 열 수 있는 문이 갱신되었기 때문.
                     key.add(Character.toUpperCase(k));
-                    arr1[ny][nx] = '.';
+                    arr1[ny][nx] = '.'; // 배열에 키 없애기.
                     qu.clear();
                     vi = new boolean[Y + 2][X + 2];
                 }
@@ -49,7 +48,8 @@ public class Main {
                         vi[ny][nx] = true;
                         qu.offer(new node(nx, ny));
                     } else {
-                        // 문서가 있을때.
+                        // 문서가 있을때 또는 문이 있을때
+                        // 문을 열수 있거나 or 문서가있는경우 수행한다.
                         if (arr1[ny][nx] == '$' || key.contains(arr1[ny][nx])) {
                             if (arr1[ny][nx] == '$') cnt++;
 
@@ -65,20 +65,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
-
+        // 입출력
         while (T-- > 0) {
-            map = new HashMap<>();
-            key = new HashSet<>();
+            key = new HashSet<>(); // 가지고 있는 키 입력
             cnt = 0;
 
             st = new StringTokenizer(br.readLine(), " ");
             Y = Integer.parseInt(st.nextToken());
             X = Integer.parseInt(st.nextToken());
-            arr1 = new char[Y + 2][X + 2];
-
+            arr1 = new char[Y + 2][X + 2]; // 빌딩 안팎으로 이동가능.
+            // 빌딩 바깥쪽 설정.
             for (int i = 0; i < Y + 2; i++) {
                 Arrays.fill(arr1[i], '.');
             }
@@ -90,7 +90,7 @@ public class Main {
                 }
             }
 
-            String keys = br.readLine();
+            String keys = br.readLine(); // 가지고 있는 키 입력
 
             if (!(keys.equals("0"))) {
                 for (int i = 0; i < keys.length(); i++) {
@@ -99,7 +99,8 @@ public class Main {
             }
 
             bfs(0, 0);
-            System.out.println(cnt);
+            sb.append(cnt).append("\n");
         }
+        System.out.print(sb);
     }
 }
