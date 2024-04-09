7ae8b9d7b1405d3011cdd49a25fc7dedd7228d81
영석 <149328708+DevKoben@users.noreply.github.com>
2024-04-09 13:10:34 +0900

    [Gold I] Title: 비숍, Time: 320 ms, Memory: 198664 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.java
+++ 백준/Gold/1799. 비숍/비숍.java
@@ -5,38 +5,31 @@ public class Main {
     public static int N, max;
     public static int[][] arr1;
 
-
     public static void dfs(int idx, int cnt) {
-        // 보드에 가능한 비숍 다 올렸다면 값 구하기.
+        // 보드 끝까지 탐색 했다면 max 값 도출
         if (idx >= N * N) {
             max = Math.max(max, cnt);
             return;
         }
-        // 일차원 배열 좌표 idx를 2차원 좌표로 변환
+        // 1차원 배열 인덱스 -> 2차원 배열 인덱스로 변환
         int x = idx % N;
         int y = idx / N;
-        int nIdx = calcIdx(idx); //칸 전진.
-
-        if (arr1[y][x] == 0) {
-            dfs(idx + nIdx, cnt);
-            return;
-        }
-
+        int nIdx = calcIdx(idx); // 다음 전진 횟수 결정.
 
         if (arr1[y][x] == 1 && isPlaced(x, y)) {
-
             arr1[y][x] = 2;
             dfs(idx + nIdx, cnt + 1);
             arr1[y][x] = 1;
         }
         dfs(idx + nIdx, cnt);
     }
-    
-    // 인덱스 칸 전진, N이 짝수, 홀수에 비례하게.
-    public static int calcIdx(int idx) {
 
+    // 보드 크기 짝수, 홀수 일때 이동 길이 조절.
+    public static int calcIdx(int idx) {
+        // 홀수라면 그냥 2칸++
         if (N % 2 == 1) return 2;
 
+        // 짝수라면 색깔에 맞는 다음칸 +n 리턴
         if (idx % N == N - 1) return 1;
         else if (idx % N == N - 2) return 3;
         else return 2;
@@ -47,9 +40,7 @@ public class Main {
         // 좌상, 우상, 좌하, 우하
         int[] dx = {-1, 1, -1, 1};
         int[] dy = {-1, -1, 1, 1};
-        
-        // 각 대각선에 비숍이 놓아져 있는지 판별.
-        
+        // 현재 좌표기준으로 비숍 이동경로 겹치는 다른 비숍있는지 판단.
         for (int i = 0; i < 4; i++) {
             int flag = 1;
             while (true) {
@@ -63,6 +54,7 @@ public class Main {
         return true;
     }
 
+    // 좌표의 범위 체크
     public static boolean isRange(int x, int y) {
         return x >= 0 && y >= 0 && x < N && y < N;
     }
@@ -81,18 +73,16 @@ public class Main {
                 arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int res = 0;
-        // 검정칸과 흰색칸에 있는 비숍은 절대 만날 수 없음.
-        // 백트래킹을 칸색깔 별로 나눠서 탐색한다.
+        // 검정칸, 흰색칸 나눠서 탐색 -> 검정 비숍과 흰색 비숍은 만날일 없기때문에 가능함.
         // 검정칸 탐색
+        int res = 0;
         dfs(0, 0);
         res += max;
         max = 0;
-        // 흰색 칸 탐색
+
+        // 흰색칸 탐색
         dfs(1, 0);
         res += max;
-
         System.out.println(res);
     }
 }
