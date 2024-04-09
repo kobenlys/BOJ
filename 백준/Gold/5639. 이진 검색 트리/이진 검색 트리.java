9d7ab5a668cb32d6d3d87375247ebc82472cd351
영석 <149328708+DevKoben@users.noreply.github.com>
2024-04-10 02:42:06 +0900

    [Gold V] Title: 이진 검색 트리, Time: 460 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static StringBuilder sb = new StringBuilder();
 
     public static class node {
         int val;
@@ -13,8 +14,9 @@ public class Main {
             this.right = null;
         }
 
+        // 전위순회 순으로 입력받는다면
         public void insert(int n) {
-
+            // 상위 노드 보다 n이 작다면 left노드에 입력
             if (n < val) {
                 if (this.left == null) {
                     this.left = new node(n);
@@ -22,6 +24,7 @@ public class Main {
                     this.left.insert(n);
                 }
             } else {
+                // 상위노드 보다 n이 크다면 right노드에 입력
                 if (this.right == null) {
                     this.right = new node(n);
                 } else {
@@ -31,28 +34,32 @@ public class Main {
         }
     }
 
+    // 후위순회 탐색 + 출력
     public static void postorder(node nd) {
         if (nd == null) return;
         postorder(nd.left);
         postorder(nd.right);
-        System.out.println(nd.val);
+        sb.append(nd.val).append("\n");
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        // 루트노드 초기 생성.
         node root = new node(Integer.parseInt(br.readLine()));
-        String input = "";
+
         while (true) {
-            input = br.readLine();
+            String input = br.readLine();
             if (input == null || input.isEmpty()) {
                 break;
             }
+            // 자식노드 설정.
             int N = Integer.parseInt(input);
             root.insert(N);
         }
-
+        // 후위순회 탐색
         postorder(root);
+        System.out.print(sb);
     }
 }
