c1e4b9f99286a94145c86ddd4c3e2ed49545dd10
영석 <149328708+DevKoben@users.noreply.github.com>
2024-04-14 23:31:59 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 888 ms, Memory: 221224 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class Main {
     public static int N, M;
-    public static int[][] arr1;
-    public static int[][] vi;
-    public static int[][] union;
-
+    public static int[][] arr1; // 맵
+    public static int[][] vi; // 방문 처리 + 공백체크
+    public static int[][] union; // 같은 집합인지 표시
+    
+    // 좌표 객체
     public static class node {
         int x, y;
 
@@ -15,16 +16,17 @@ public class Main {
             this.y = y;
         }
     }
-
+    
+    // 공백 전처리 -> 공백 카운트 하기.
     public static void preProcess(int x, int y, int flag) {
         Queue<node> qu = new LinkedList<>();
         Stack<node> stk = new Stack<>();
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
-        int cnt = 1;
         qu.offer(new node(x, y));
         stk.push(new node(x, y));
         vi[y][x] = -1;
+        int cnt = 1;
 
         while (!qu.isEmpty()) {
 
@@ -37,38 +39,39 @@ public class Main {
                 if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
 
                 if (arr1[ny][nx] == 0 && vi[ny][nx] == 0) {
-                    cnt++;
-                    vi[ny][nx] = -1;
-                    stk.push(new node(nx, ny));
-                    qu.offer(new node(nx, ny));
+                    cnt++; // 이동 가능한 공백 카운트
+                    vi[ny][nx] = -1; // 일단 방문처리.
+                    stk.push(new node(nx, ny)); // 이동한 칸 좌표 입력
+                    qu.offer(new node(nx, ny)); // 큐에 다음 이동넣기.
                 }
             }
         }
 
         for (node nd : stk) {
-            vi[nd.y][nd.x] = cnt;
-            union[nd.y][nd.x] = flag;
+            vi[nd.y][nd.x] = cnt; // 이동한 칸 마다 총 공백수 넣기.
+            union[nd.y][nd.x] = flag; // 같은 집합 표시.
         }
     }
-
+    
+    // 이동 할 수 있는 칸 체크
     public static int cntSpace(int x, int y) {
         HashSet<Integer> set = new HashSet<>();
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
         int ans = 1;
-
+        // vi배열 상하좌우 숫자 ans에 더하기.
         for (int i = 0; i < 4; i++) {
 
             int nx = x + dx[i];
             int ny = y + dy[i];
-
+            // 범위에 맞지 안으면 continue + 같은 집합에 속한 공백은 다시 카운트 x
             if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
             if (set.contains(union[ny][nx]) || union[ny][nx] == 0) continue;
 
             set.add(union[ny][nx]);
             ans += vi[ny][nx];
         }
-        
+        // 10으로 나눈 수 출력해야함.
         return ans % 10;
     }
 
@@ -94,7 +97,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (arr1[i][j] == 0 && vi[i][j] == 0) {
-                    preProcess(j, i, cnt++);
+                    preProcess(j, i, cnt++); // 전처리 + 같은 집합 표시
                 }
             }
         }
