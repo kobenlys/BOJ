764d05780cfde4682f96eb16786214fd74a13727
영석 <149328708+DevKoben@users.noreply.github.com>
2024-04-29 00:19:24 +0900

    [Gold IV] Title: 팰린드롬?, Time: 736 ms, Memory: 231184 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -4,33 +4,24 @@ import java.util.*;
 public class Main {
     public static int N;
     public static int[] arr1;
-    public static int[] dp;
+    public static boolean[][] dp;
 
-    public static boolean isPalindrome(int start, int end, boolean isOdd) {
+    public static void isPalindrome() {
 
-        int left = 0, right = 0;
-        int mid = (start + end) / 2;
-
-        boolean isOk = true;
-
-        if (isOdd) {
-            left = mid;
-            right = mid + 1;
-        } else {
-            left = right = mid;
-        }
-
-        while (left >= start && right <= end) {
-
-            if (arr1[left] != arr1[right]) {
-                isOk = false;
-                break;
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = true;
+            if (i != N - 1 && arr1[i] == arr1[i+1]) {
+                dp[i][i + 1] = true;
             }
-            left--;
-            right++;
         }
 
-        return isOk;
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                if (arr1[j] == arr1[j + i] && dp[j + 1][i + j - 1]) {
+                    dp[j][j+i] = true;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,7 +31,7 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         arr1 = new int[N];
-        dp = new int[N];
+        dp = new boolean[N][N];
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
@@ -48,26 +39,14 @@ public class Main {
         }
 
         int M = Integer.parseInt(br.readLine());
+        isPalindrome();
 
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
 
-            int mid = (start + end) / 2;
-
-            if (start == end) {
-                sb.append(1).append("\n");
-                continue;
-            }
-
-            if ((start + end) % 2 != 0) {
-                // 홀수
-                sb.append(isPalindrome(start, end, true) ? 1 : 0).append("\n");
-            } else {
-                // 짝수
-                sb.append(isPalindrome(start, end, false) ? 1 : 0).append("\n");
-            }
+            sb.append(dp[start][end] ? 1 : 0).append("\n");
 
         }
         System.out.print(sb);
