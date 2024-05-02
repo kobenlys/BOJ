9faffc9543098184a94ad245034c8d27af048759
영석 <149328708+DevKoben@users.noreply.github.com>
2024-05-03 08:49:44 +0900

    [Gold II] Title: 버블 소트, Time: 732 ms, Memory: 61312 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
+
     // 번호와, 원래 위치 인덱스 저장
     public static class node implements Comparable<node> {
         int n, idx;
@@ -11,33 +11,38 @@ public class Main {
             this.n = n;
             this.idx = idx;
         }
+
         @Override
         public int compareTo(node o) {
+            // 우선순위 큐는 같은 값에 대해 인덱스 순서가 보장이 안된다.
+            if (n == o.n) {
+                return idx - o.idx;
+            }
             return n - o.n;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<node> pq = new PriorityQueue<>();
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         int answer = 0;
-        node[] arr1 = new node[N];
+        int idx = 0;
 
+        // 오름차순 정렬 + 입력
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
-            arr1[i] = new node(num, i);
+            pq.offer(new node(num, i));
         }
-        // 오름차순 정렬 
-        Arrays.sort(arr1);
-        
-        for (int i = 0; i < N; i++) {
-            node nd = arr1[i];
+
+        while (!pq.isEmpty()) {
+            node nd = pq.poll();
             // 원래 있던 자리 - 정렬 후 자리 인덱스의 차가 가장 큰 것을 저장
-            answer = Math.max(answer, nd.idx - i);
+            // 오른쪽으로 이동한 요소들 기준으로 카운트
+            answer = Math.max(answer, nd.idx - idx++);
         }
-        // 버블 정렬 특징과 관련있음
-        System.out.print(answer + 1);
+        System.out.println(answer + 1);
     }
 }
