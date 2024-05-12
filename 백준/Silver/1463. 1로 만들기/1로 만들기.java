872ff3109c351c68c808a0972558e10aff18e89e
영석 <149328708+DevKoben@users.noreply.github.com>
2024-05-12 22:15:44 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15516 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,45 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static Integer[] dp;
 
-    public static int algorithm(int N) {
-
-        if (dp[N] == null) {
-
-            if (N % 6 == 0) {
-                dp[N] = Math.min(algorithm(N - 1), Math.min(algorithm(N / 3), algorithm(N / 2))) + 1;
-            }
-            else if (N % 3 == 0) {
-                dp[N] = Math.min(algorithm(N / 3), algorithm(N - 1)) +1;
-            }
-            else if (N % 2 ==0) {
-                dp[N] = Math.min(algorithm(N / 2), algorithm(N - 1)) + 1;
-            }
-            else {
-                dp[N] = algorithm(N - 1) +1;
-
-            }
-        }
-        return dp[N];
-
-    }
-
-
-    public static void main(String[] args) throws IOException { //조건 입력
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
 
-        dp = new Integer[N + 1];
+        for (int i = 2; i <= N; i++) {
 
-        dp[0] = dp[1] = 0;
-
-        System.out.println(algorithm(N));
-
+            dp[i] = dp[i - 1] + 1;
+            
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        System.out.println(dp[N]);
     }
 }
