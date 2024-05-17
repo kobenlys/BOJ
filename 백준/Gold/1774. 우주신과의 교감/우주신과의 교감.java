8c4ada13a5ce9cff3f2931fdf3e03daee6e0c64a
영석 <149328708+DevKoben@users.noreply.github.com>
2024-05-17 19:51:10 +0900

    [Gold III] Title: 우주신과의 교감, Time: 436 ms, Memory: 36708 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -13,18 +13,20 @@ public class Main {
             this.e = e;
             this.val = val;
         }
-
         @Override
         public int compareTo(node o) {
+            // 정수형이 아닌 다른 타입은 Double, Long.compare 사용하기.
             return Double.compare(val, o.val);
         }
     }
-
+    
+    // 부모 찾기
     public static int find(int x) {
         if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
-
+    
+    // 같은 집합에 합치기.
     public static boolean union(int x, int y) {
         int from = find(x);
         int to = find(y);
@@ -63,27 +65,27 @@ public class Main {
             int sIdx = Integer.parseInt(st.nextToken()) - 1;
             int eIdx = Integer.parseInt(st.nextToken()) - 1;
             vi[sIdx][eIdx] = true;
-            union(sIdx, eIdx);
+            union(sIdx, eIdx); // 이미 이어진 길 통로 이어주기.
         }
-
+        
+        // 각 노드별 가능한 경로 만들기
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                if(vi[i][j]) continue;
-
+                if(vi[i][j]) continue; // 원래 이어진 통로 제거
+                // 유클리드 거리로 거리 측정
                 double v = Math.sqrt(Math.pow(arr1[i][0] - arr1[j][0], 2)
                         + Math.pow(arr1[i][1] - arr1[j][1], 2));
                 pq.offer(new node(i, j, v));
             }
         }
-
+        // 크루스칼 알고리즘.
         while (!pq.isEmpty()) {
             node nd = pq.poll();
-
             if (!union(nd.s, nd.e)) {
                 ans += nd.val;
             }
         }
-
+        // 소수 둘째 자리까지 출력한다.
         System.out.printf("%.2f%n", ans);
     }
 }
