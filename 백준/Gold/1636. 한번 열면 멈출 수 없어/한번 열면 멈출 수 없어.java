5ca9cb6c28712b43c0035693f290e2232600e5f6
영석 <149328708+DevKoben@users.noreply.github.com>
2024-05-17 17:04:31 +0900

    [Gold IV] Title: 한번 열면 멈출 수 없어, Time: 532 ms, Memory: 94792 KB -BaekjoonHub

--- 백준/Gold/1636. 한번 열면 멈출 수 없어/한번 열면 멈출 수 없어.java
+++ 백준/Gold/1636. 한번 열면 멈출 수 없어/한번 열면 멈출 수 없어.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main {
     public static int N;
     public static int[][] arr1;
-
+    
+    // 구현
     public static int[] algorithm(int tmp) {
         int[] list = new int[N + 1];
         int answer = 0;
@@ -14,15 +15,17 @@ public class Main {
 
             int tmpS = arr1[i][0];
             int tmpE = arr1[i][1];
-
+            // 절댓값 측정
             int tmp1 = Math.abs(tmp - tmpS);
             int tmp2 = Math.abs(tmp - tmpE);
-
+            
+            // tmp가 범위 안이라면 조절할 필요없음.
             if (tmpS <= tmp && tmp <= tmpE) {
                 list[i + 1] = tmp;
                 continue;
             }
-
+            
+            // 절댓값이 가장 작은 범위로 조절한다.
             if (tmp1 > tmp2) {
                 list[i + 1] = tmpE;
                 tmp = tmpE;
@@ -33,7 +36,7 @@ public class Main {
                 answer += tmp1;
             }
         }
-
+        // 가장 앞 예상수명을 저장후 리턴.
         list[0] = answer;
         return list;
     }
@@ -51,29 +54,25 @@ public class Main {
             arr1[i][0] = Integer.parseInt(st.nextToken());
             arr1[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        if (N == 1) {
-            System.out.println(0);
-            System.out.println(arr1[0][0]);
-            System.exit(0);
-        }
-
+        
         int[] answerList = null;
         int min = Integer.MAX_VALUE;
-
+        
+        // 시작 ~ 끝 까지, 범위 숫자중 하나가 가장 최적인 시작지점이다.
         for (int i = arr1[0][0]; i <= arr1[0][1]; i++) {
             int[] res = algorithm(i);
 
+            // 최솟값으로 배열 갱신
             if (min > res[0]) {
                 min = res[0];
                 answerList = res;
             }
         }
 
+        // 출력
         for (int e : answerList) {
             sb.append(e).append("\n");
         }
-
 
         System.out.print(sb);
     }
