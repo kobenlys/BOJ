5c789fc4eb37730b8342f2a11b5917444649367b
영석 <149328708+kobenlys@users.noreply.github.com>
2024-05-20 01:09:17 +0900

    [Bronze V] Title: 등록, Time: 68 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        System.out.println(490);
+        System.out.println("vlfxhd69");
+    }
+}
