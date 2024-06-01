d4ab5d3d23e3873ca745c2e8a894298b844d79ee
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-02 02:36:58 +0900

    [Platinum V] Title: 발전소, Time: 128 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Platinum/1102. 발전소/발전소.java
+++ 백준/Platinum/1102. 발전소/발전소.java
@@ -2,62 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int N, P;
     public static int[][] arr1;
     public static int[] dp;
-    public static int N, P;
 
     public static int dfs(int cnt, int pos) {
 
-        if(cnt >= P) return 0;
-        if(dp[pos] != -1) return dp[pos];
+        if (cnt >= P) return 0;
+        if (dp[pos] != -1) return dp[pos];
 
         dp[pos] = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
-            // i 번째 발전소가 정상 작동한다면
-                if ((pos & (1 << i)) != 0) {
-                    for (int j = 0; j < N; j++) {
-                        // j 번째 발전소가 정상 작동하지 않는다면.
-                        if ((pos & (1 << j)) == 0) {
-                            // 최소값 구하기, 재귀
-                            dp[pos] = Math.min(dp[pos], dfs(cnt + 1, pos | (1 << j)) + arr1[i][j]);
-                        }
+            // 작동중인 발전소 찾기
+            if ((pos & (1 << i)) != 0) {
+
+                for (int j = 0; j < N; j++) {
+                    // 고장난 발전소 찾기
+                    if ((pos & (1 << j)) == 0) {
+                        // 이전 저장된 dp[pos]값 보다, 현재 경로(탑다운)가 더 작다면 갱신한다.
+                        dp[pos] = Math.min(dp[pos], dfs(cnt + 1, pos | (1 << j)) + arr1[i][j]);
                     }
                 }
             }
-
-            return dp[pos];
         }
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+        return dp[pos];
+    }
 
-            N = Integer.parseInt(br.readLine());
-            arr1 = new int[N][N];
-            dp = new int[1 << N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-            Arrays.fill(dp, -1);
+        N = Integer.parseInt(br.readLine());
+        arr1 = new int[N][N];
+        dp = new int[1 << N];
+        Arrays.fill(dp, -1);
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    arr1[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            String state = br.readLine();
-            int cnt = 0;
-            int pos = 0;
-
-            for (int i = 0; i < state.length(); i++) {
-                if(state.charAt(i) == 'Y'){
-                    cnt++;
-                    pos = pos | (1 << i);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        String str = br.readLine();
         P = Integer.parseInt(br.readLine());
+        int pos = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'Y') {
+                cnt++;
+                pos = pos | (1 << i);
+            }
+        }
+
         int answer = dfs(cnt, pos);
         System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
     }
