9d844a8113e3525dacfc8929e159a9a25b58bb03
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-04 22:39:29 +0900

    [Bronze IV] Title: R2, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+
+        // S = N + M / 2
+        // S*2 = N + M
+
+        System.out.println(S * 2 - N);
+    }
+}
