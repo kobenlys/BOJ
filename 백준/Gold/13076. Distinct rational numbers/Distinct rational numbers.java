792a7e7b166f8b30cf6fb0eb6423526bfd896e00
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-07 02:29:49 +0900

    [Gold I] Title: Distinct rational numbers, Time: 116 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/13076. Distinct rational numbers/Distinct rational numbers.java
+++ 백준/Gold/13076. Distinct rational numbers/Distinct rational numbers.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class Main {
 
+    // 오일러의 피 함수.
     public static int euler_Phi(int N) {
         int res = N;
 
         for (int i = 2; i <= Math.sqrt(N); i++) {
-            if (N % i == 0) {
-                while (N % i == 0) {
+            if(N % i == 0){
+                while(N % i == 0){
                     N /= i;
                 }
                 res -= res / i;
@@ -25,13 +26,17 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
         int[] arr1 = new int[10001];
+        // 시간단축 위해 누적합구하기
         for (int i = 1; i <= 10000; i++) {
             arr1[i] = euler_Phi(i) + arr1[i - 1];
         }
 
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
+        // 분수로 표현 가능해야함. 1/2, 1/3 이렇게 하지만 2/4 는 1/2로 변환 가능 중복제거해줘야함
+        // 즉, 최대공약수가 1인 짝만 카운트 필요함
 
+        while (T-- > 0) {
+            // 출력
+            int N = Integer.parseInt(br.readLine());
             sb.append(arr1[N] + 1).append("\n");
         }
         System.out.print(sb);
