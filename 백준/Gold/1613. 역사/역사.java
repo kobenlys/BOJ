95b5374fd30b7f5f128846886b39d6d37f4cef60
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-13 04:19:06 +0900

    [Gold III] Title: 역사, Time: 528 ms, Memory: 36492 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static boolean[][] arr1;
+    public static boolean[][] arr1Rvrs;
+
+    public static void floydWarshall() {
+
+        for (int fw = 0; fw < N; fw++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) continue;
+
+                    if (arr1[i][fw] && arr1[fw][j]) {
+                        arr1[i][j] = true;
+                    }
+
+                }
+            }
+        }
+
+        for (int fw = 0; fw < N; fw++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) continue;
+
+                    if (arr1Rvrs[i][fw] && arr1Rvrs[fw][j]) {
+                        arr1Rvrs[i][j] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        arr1 = new boolean[N][N];
+        arr1Rvrs = new boolean[N][N];
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            arr1[s][e] = true;
+            arr1Rvrs[e][s] = true;
+        }
+
+        floydWarshall();
+        int S = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < S; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+
+            if (arr1[s][e]) {
+                sb.append(-1).append("\n");
+            } else if (arr1Rvrs[s][e]) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
