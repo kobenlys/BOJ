57cd804c625ca7e2689ee627a35630d2df11f64a
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-18 02:33:46 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 424 ms, Memory: 40648 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -10,19 +10,20 @@ public class Main {
 
     public static int dfs(int x, int y) {
 
+        // 이미 탐색 되었더라면 리턴
         if (dp[y][x] != 0) return dp[y][x];
 
-
+        // 상 하 좌 우 탐색
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-
+            // 현재 칸보다 다음 칸이 크다면 전진
             if (arr1[ny][nx] > arr1[y][x]) {
+                // 가장 큰 값을 저장
                 dp[y][x] = Math.max(dp[y][x], dfs(nx, ny) + 1);
             }
         }
-
         return dp[y][x];
     }
 
@@ -42,11 +43,13 @@ public class Main {
             }
         }
 
+        // 각 자리별 dfs탐색
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 answer = Math.max(answer, dfs(j, i));
             }
         }
+
         System.out.println(answer + 1);
     }
 }
