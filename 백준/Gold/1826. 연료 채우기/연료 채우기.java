d05eb8450943dd31e337e646b7f86f799abd07e2
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-21 17:36:30 +0900

    [Gold II] Title: 연료 채우기, Time: 176 ms, Memory: 17388 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static class node implements Comparable<node> {
         int idx, fuel;
 
@@ -20,16 +19,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PriorityQueue<node> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> tmp = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> tmp = new PriorityQueue<>(Comparator.reverseOrder());
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            pq.offer(new node(a, b));
+            int idx = Integer.parseInt(st.nextToken());
+            int fuel = Integer.parseInt(st.nextToken());
+            pq.offer(new node(idx, fuel));
         }
 
         st = new StringTokenizer(br.readLine());
@@ -37,23 +36,18 @@ public class Main {
         int P = Integer.parseInt(st.nextToken());
         int answer = 0;
 
-        while (!pq.isEmpty()) {
-            if (P >= L) {
-                break;
-            }
+        while (!pq.isEmpty() && P < L) {
+
 
             while (!pq.isEmpty() && P >= pq.peek().idx) {
                 tmp.add(pq.poll().fuel);
             }
 
-            if (!tmp.isEmpty()) {
-                answer++;
-                P += tmp.poll();
-            } else {
-                break;
-            }
-        }
+            if (tmp.isEmpty()) break;
 
+            answer++;
+            P += tmp.poll();
+        }
         System.out.println(P >= L ? answer : -1);
     }
 }
