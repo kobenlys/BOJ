4ace36ae66a22690be70a585b7e04ca7544ad7f7
영석 <149328708+kobenlys@users.noreply.github.com>
2024-06-23 03:18:16 +0900

    [Gold III] Title: 보석, Time: 144 ms, Memory: 13636 KB -BaekjoonHub

--- 백준/Gold/2492. 보석/보석.java
+++ 백준/Gold/2492. 보석/보석.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    
+    // 금강석 위치 담는 클래스
     public static class node {
         int x, y;
 
@@ -34,33 +35,38 @@ public class Main {
             int y = Integer.parseInt(st.nextToken());
             arr1.add(new node(x, y));
         }
-
+        
+        // pos1, pos2를 정사각형 변에 걸치게 설정 후 범위 내 금강석 탐색
         for (node pos1 : arr1) {
             for (node pos2 : arr1) {
                 int tmpCnt = 0;
 
                 int tX = pos1.x;
                 int tY = pos2.y;
-
+                
+                // 문제조건 : 정사각형의 범위가 지도 밖으로 나가는 것 불가.
+                // 범위를 넘는다면 범위를 수정 해 준다.
                 if (tX + K > N) tX = N - K;
                 if (tY + K > M) tY = M - K;
 
                 for (node tmp : arr1) {
+                    // 정사각형 범위 내 금강석이 존재하는지 탐색.
                     if (tX <= tmp.x && tX + K >= tmp.x
                             && tY <= tmp.y && tY + K >= tmp.y) {
                         tmpCnt++;
                     }
                 }
-                
-                if (cnt <= tmpCnt) {
+                // 최적값 발견시 업데이트
+                if (cnt < tmpCnt) {
                     cnt = tmpCnt;
+                    // 왼쪽 상단 꼭짓점.
                     answerX = tX;
                     answerY = tY + K;
-                    if (answerY > M) answerY = M;
                 }
             }
         }
-
+        
+        // 출력
         sb.append(answerX).append(" ").append(answerY).append("\n");
         sb.append(cnt);
         System.out.print(sb);
