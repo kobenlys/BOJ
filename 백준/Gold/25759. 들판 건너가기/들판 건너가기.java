e340ad935c53e94e1698b6ff9b6d9015fc53487b
영석 <149328708+kobenlys@users.noreply.github.com>
2024-07-05 00:11:04 +0900

    [Gold IV] Title: 들판 건너가기, Time: 364 ms, Memory: 27276 KB -BaekjoonHub

--- 백준/Gold/25759. 들판 건너가기/들판 건너가기.java
+++ 백준/Gold/25759. 들판 건너가기/들판 건너가기.java
@@ -7,27 +7,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int ans = 0;
-        
+        int N = Integer.parseInt(br.readLine());
         int[] dp = new int[101];
         Arrays.fill(dp, -1);
 
         st = new StringTokenizer(br.readLine());
-        int tmp = Integer.parseInt(st.nextToken());
-        dp[tmp] = 0;
+        int n = Integer.parseInt(st.nextToken());
+
+        dp[n] = 0; // 가장 첫 시작 입력
 
         for (int i = 1; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
             for (int j = 1; j <= 100; j++) {
-                if (dp[j] > -1) {
+                if (dp[j] > -1) { // 아직 입력되지 않아 짝을 이룰 수 없는 숫자 제외
+                    // dp[num] 의 숫자를 가장 큰 숫자로 유지한다.
+                    // dp[j] + (num - j) + (num - j)
+                    // 문제조건은 (num - j) ^2 으로 값을 구하고 dp[j](이전 결과값)을 더해준다.
                     dp[num] = Math.max(dp[num], dp[j] + (num - j) * (num - j));
                 }
             }
         }
-
-        ans = Arrays.stream(dp).max().getAsInt();
-        System.out.println(ans);
+        // dp배열중 최댓값 구하기.
+        int answer = Arrays.stream(dp).max().getAsInt();
+        System.out.println(answer);
     }
 }
