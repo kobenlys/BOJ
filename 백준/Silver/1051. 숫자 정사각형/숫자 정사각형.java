aef6a84236c43f25df47c28a0824027f8aaa56e3
영석 <149328708+kobenlys@users.noreply.github.com>
2024-07-24 20:25:39 +0900

    [Silver III] Title: 숫자 정사각형, Time: 84 ms, Memory: 11860 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -1,54 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N, M, max;
+    public static int N, M;
     public static int[][] arr1;
 
-    public static void algorithm() {
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int a = arr1[i][j]; // 좌측 상단 꼭짓점 기준
-                int cnt = 0;
-                for (int k = j; k < M; k++, cnt++) { // 해당 좌표 에서 M-1 까지 탐색
-                    if (range(j, i, cnt)) {
-                        // cnt 통해 1*1 정사각형 부터 탐색
-                        int b = arr1[i][j + cnt]; // 우측 상단 꼭짓점
-                        int c = arr1[i + cnt][j]; // 좌측 하단 꼭짓점
-                        int d = arr1[i + cnt][j + cnt]; // 우측 하단 꼭짓점
+    public static int findRectangle(int x, int y) {
+        int max = 1;
+        int target = arr1[y][x];
 
-                        if (a == b && a == c && a == d) {
-                            max = Math.max(max, cnt);
-                        }
-                    }
+        for (int i = 1; i < N; i++) {
+            if (x + i >= M || y + i >= N) break;
+            if (target == arr1[y][x + i]) {
+                if (arr1[y][x + i] == arr1[y + i][x + i] && arr1[y + i][x + i] == arr1[y + i][x]) {
+                    max = i + 1;
                 }
             }
         }
+        return max * max;
     }
 
-    public static boolean range(int x, int y, int size) { // 좌표 범위 체크
-        return x + size >= 0 && y + size >= 0 && x + size < M && y + size < N;
-    }
-
-    public static void main(String[] args) throws IOException { //조건 입력
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        int answer = 0;
         arr1 = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr1[i][j] = Character.getNumericValue(str.charAt(j));
+                arr1[i][j] = Character.getNumericValue(input.charAt(j));
             }
         }
-        // 함수 실행, 정사각형 크기 출력
-        algorithm();
-        System.out.print(++max * max);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                answer = Math.max(answer, findRectangle(j, i));
+            }
+        }
+        System.out.println(answer);
     }
 }
