84e2fa0bec2049df4abd64b3638b60cc1f8b0ef7
영석 <149328708+kobenlys@users.noreply.github.com>
2024-08-10 15:09:20 +0900

    [Gold III] Title: 아기 상어, Time: 120 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,105 +1,108 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N, size, cnt, tmp;
+    public static int N;
     public static int[][] arr1;
-    public static boolean[][] vi;
-    public static Queue<node> qu = new LinkedList<>();
-    public static PriorityQueue<node> pq = new PriorityQueue<>(new Comparator<node>() {
-        @Override
-        public int compare(node o1, node o2) {
-            if (o1.step != o2.step) {
-                return o1.step - o2.step;
-            } else if (o1.y != o2.y) {
-                return o1.y - o2.y;
-            } else {
-                return o1.x - o2.x;
-            }
-        }
-    });
+    public static PriorityQueue<node> pq = new PriorityQueue<>();
+    public static Queue<node> qu = new ArrayDeque<>();
 
-    public static class node {
-        int x, y, step;
+    public static class node implements Comparable<node> {
+        int x, y, nowSize, eatCnt, step;
 
-        public node(int x, int y, int step) {
+        public node(int x, int y, int nowSize, int eatCnt, int step) {
             this.x = x;
             this.y = y;
+            this.nowSize = nowSize;
+            this.eatCnt = eatCnt;
             this.step = step;
         }
-    }
 
-    public static void bfs(int x, int y) {
-        findFish(x, y);
+        @Override
+        public int compareTo(node o) {
 
-        while (!pq.isEmpty()) {
-            node site = pq.poll();
-            cnt += site.step;
-            arr1[site.y][site.x] = 0;
-            tmp++;
-            if (size == tmp) {
-                size++;
-                tmp = 0;
+            if (step == o.step) {
+                if (y == o.y) {
+                    return x - o.x;
+                }
+                return y - o.y;
             }
-            findFish(site.x, site.y);
+            return step - o.step;
         }
     }
 
-    public static void findFish(int x, int y) {
-        int[] dx = {0, -1, 1, 0}; // 상 좌 우 하
-        int[] dy = {-1, 0, 0, 1};
-        pq.clear();
-        qu.clear();
-        vi = new boolean[N][N];
-        qu.offer(new node(x, y, 0));
+    public static void bfs() {
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        boolean[][] vi = new boolean[N][N];
 
         while (!qu.isEmpty()) {
+
             node nd = qu.poll();
+
             for (int i = 0; i < 4; i++) {
                 int nx = nd.x + dx[i];
                 int ny = nd.y + dy[i];
                 int nStep = nd.step + 1;
-
                 if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+
                 if (!vi[ny][nx]) {
 
-                    if (arr1[ny][nx] != 0 && arr1[ny][nx] < size) {
+                    if (arr1[ny][nx] == 0 || arr1[ny][nx] == nd.nowSize) {
                         vi[ny][nx] = true;
-                        pq.offer(new node(nx, ny, nStep));
-                    } else if (arr1[ny][nx] == 0 || arr1[ny][nx] == size) {
+                        qu.offer(new node(nx, ny, nd.nowSize, nd.eatCnt, nStep));
+                        continue;
+                    }
+
+                    if (arr1[ny][nx] != 0 && arr1[ny][nx] < nd.nowSize) {
+                        pq.offer(new node(nx, ny, nd.nowSize, nd.eatCnt, nStep));
                         vi[ny][nx] = true;
-                        qu.offer(new node(nx, ny, nStep));
                     }
                 }
             }
         }
     }
 
-
-    public static void main(String[] args) throws IOException { //조건 입력
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
+        int day = 0;
         arr1 = new int[N][N];
-        vi = new boolean[N][N];
-        int x = 0, y = 0;
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr1[i][j] = Integer.parseInt(st.nextToken());
                 if (arr1[i][j] == 9) {
-                    x = j;
-                    y = i;
                     arr1[i][j] = 0;
+                    qu.offer(new node(j, i, 2, 0, 0));
                 }
             }
         }
-        size = 2;
-        tmp = 0;
-        bfs(x, y);
-        System.out.println(cnt);
+
+        while (true) {
+            bfs();
+
+            if (pq.isEmpty()) {
+                System.out.println(day);
+                System.exit(0);
+            }
+
+            node shark = pq.poll();
+            arr1[shark.y][shark.x] = 0;
+            shark.eatCnt += 1;
+            if (shark.nowSize == shark.eatCnt) {
+                shark.nowSize += 1;
+                shark.eatCnt = 0;
+            }
+            day += shark.step;
+            shark.step = 0;
+            qu.offer(shark);
+            pq.clear();
+        }
+
     }
 }
