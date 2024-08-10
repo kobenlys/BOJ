5e1550c407a739da5135d165bfa6146474443538
영석 <149328708+kobenlys@users.noreply.github.com>
2024-08-10 15:34:58 +0900

    [Gold III] Title: 아기 상어, Time: 120 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -6,7 +6,8 @@ public class Main {
     public static int[][] arr1;
     public static PriorityQueue<node> pq = new PriorityQueue<>();
     public static Queue<node> qu = new ArrayDeque<>();
-
+    
+    // 물고기 담는 객체
     public static class node implements Comparable<node> {
         int x, y, nowSize, eatCnt, step;
 
@@ -21,9 +22,9 @@ public class Main {
         @Override
         public int compareTo(node o) {
 
-            if (step == o.step) {
-                if (y == o.y) {
-                    return x - o.x;
+            if (step == o.step) { // 첫번째 우선순위 : 최단거리
+                if (y == o.y) { // 두번째 우선순위 : 가장 위에 있는 상어
+                    return x - o.x; // 3번째 우선순위 : 가장 왼쪽에 있는 상어
                 }
                 return y - o.y;
             }
@@ -48,14 +49,17 @@ public class Main {
 
 
                 if (!vi[ny][nx]) {
-
+                    
+                    // 상어는 빈칸과 크기가 같은 물고기가 있는 칸을 지나갈 수 있음
                     if (arr1[ny][nx] == 0 || arr1[ny][nx] == nd.nowSize) {
                         vi[ny][nx] = true;
                         qu.offer(new node(nx, ny, nd.nowSize, nd.eatCnt, nStep));
                         continue;
                     }
-
+                    
+                    // 크기가 나보다 작은 물고기를 먹는다
                     if (arr1[ny][nx] != 0 && arr1[ny][nx] < nd.nowSize) {
+                        // 가장 우선순위인 물고기를 판별하기 위해 우선순위 큐에 담는다.
                         pq.offer(new node(nx, ny, nd.nowSize, nd.eatCnt, nStep));
                         vi[ny][nx] = true;
                     }
@@ -86,23 +90,24 @@ public class Main {
         while (true) {
             bfs();
 
-            if (pq.isEmpty()) {
+            if (pq.isEmpty()) { // 더이상 먹을 상어가 없다면 출력 후 종료
                 System.out.println(day);
                 System.exit(0);
             }
 
             node shark = pq.poll();
-            arr1[shark.y][shark.x] = 0;
-            shark.eatCnt += 1;
-            if (shark.nowSize == shark.eatCnt) {
-                shark.nowSize += 1;
+            arr1[shark.y][shark.x] = 0; // 먹은 물고기 제거
+            shark.eatCnt += 1; // 가장 우선순위를 가지는 상어 물고기 먹음 처리
+
+            if (shark.nowSize == shark.eatCnt) { // 상어크기 == 물고기 먹은 개수 같다면
+                shark.nowSize += 1; // 사이즈 키우기
                 shark.eatCnt = 0;
             }
-            day += shark.step;
-            shark.step = 0;
-            qu.offer(shark);
-            pq.clear();
-        }
 
+            day += shark.step; // 1칸당 1초 == 최단거리 는 걸리는 시간
+            shark.step = 0;
+            qu.offer(shark); // 다음 상어BFS 탐색으로 먹을 수 있는 물고기 탐색
+            pq.clear(); // 다음 정렬을 위해 우선순위 큐 클리어
+        }
     }
 }
