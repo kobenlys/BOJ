b4e601fc57974c12d0510fc3945de35c8ffb7cab
영석 <149328708+kobenlys@users.noreply.github.com>
2024-08-24 19:31:56 +0900

    [Gold I] Title: 최솟값, Time: 424 ms, Memory: 57028 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] arr1;
+    public static int[] segment;
+
+    public static int getMid(int s, int e) {
+        return (s + e) / 2;
+    }
+
+    public static int init(int start, int end, int idx) {
+
+        if (start == end) return segment[idx] = arr1[start];
+        int mid = getMid(start, end);
+        return segment[idx] = Math.min(init(start, mid, idx * 2), init(mid + 1, end, idx * 2 + 1));
+    }
+
+    public static void update(int start, int end, int idx, int id, int val) {
+        if (id < start || id > end) return;
+
+        if (start == end) {
+            segment[idx] = val;
+            return;
+        }
+
+        int mid = getMid(start, end);
+        update(start, mid, idx * 2, id, val);
+        update(mid + 1, end, idx * 2 + 1, id, val);
+        segment[idx] = Math.min(segment[idx * 2], segment[idx * 2 + 1]);
+    }
+
+    public static int getMin(int start, int end, int idx, int left, int right) {
+        if(right < start || left > end) return Integer.MAX_VALUE;
+        if(left <= start && right >= end) return segment[idx];
+
+        int mid = getMid(start, end);
+        return Math.min(getMin(start, mid, idx * 2, left, right),
+                getMin(mid + 1, end, idx * 2 + 1, left, right));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr1 = new int[N + 1];
+        segment = new int[(N + 1) * 4];
+
+        for (int i = 1; i <= N; i++) {
+            arr1[i] = Integer.parseInt(br.readLine());
+        }
+        init(1, N, 1);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(getMin(1, N, 1, s, e)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
