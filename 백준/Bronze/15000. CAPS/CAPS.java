81635310eea5d74d91106a7579aeb9bff8142081
영석 <149328708+kobenlys@users.noreply.github.com>
2024-09-11 01:03:37 +0900

    [Bronze V] Title: CAPS, Time: 188 ms, Memory: 23800 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase());
+
+    }
+}
