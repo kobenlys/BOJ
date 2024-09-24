ccc3f51f65cc63824f5236479e5978da3f9167ad
영석 <149328708+kobenlys@users.noreply.github.com>
2024-09-25 04:47:25 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String str = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
