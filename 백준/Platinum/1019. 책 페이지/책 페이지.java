282a8321093d99e674dfefc3e2b4669800e85756
영석 <149328708+kobenlys@users.noreply.github.com>
2024-10-20 23:48:40 +0900

    [Platinum V] Title: 책 페이지, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Platinum/1019. 책 페이지/책 페이지.java
+++ 백준/Platinum/1019. 책 페이지/책 페이지.java
@@ -4,35 +4,29 @@ import java.util.*;
 public class Main {
 
     public static int[] countDigit(int N) {
+
         int[] res = new int[10];
-        int cur = 1;
+        int target = 1;
 
-        // cur = 1;
-        // 19
-        // 19 - ( 19 / 1) * 1 -> 0
-        // (19/1) % 10 -> 9
-        // 19 / (cur * 10)  -> 1
+        while (N / target > 0) {
 
-        while (N / cur > 0) {
+            int lower = N - (N / target) * target;
+            int now = (N / target) % 10;
+            int higher = N / (target * 10);
 
-            int low = N - (N / cur) * cur; // 현재 자릿수 하위 숫자들
-            int now = (N / cur) % 10; // 현재자리 수
-            int high = N / (cur * 10); // 현재 자리의 상위 숫자들
-
-            // 상위 숫자에 따라 발생 빈도 계산
             for (int i = 0; i < 10; i++) {
-                res[i] += high * cur;
+                res[i] += higher * target;
             }
 
-            // 현재 자리수에 따라 발생 빈도 계산
             for (int i = 0; i < now; i++) {
-                res[i] += cur;
+                res[i] += target;
             }
 
-            res[now] += low + 1; // 현재 숫자에 대해 계산
-            res[0] -= cur;
-            cur *= 10;
+            res[now] += lower + 1;
+            res[0] -= target;
+            target *= 10;
         }
+
 
         return res;
     }
