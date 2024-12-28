36a12bf25ed210f913f937e7f371588ab46f21cd
영석 <149328708+kobenlys@users.noreply.github.com>
2024-12-29 03:29:45 +0900

    [Gold II] Title: 대표 선수, Time: 2844 ms, Memory: 326344 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -22,31 +22,34 @@ public class Main {
     public static boolean isPossible(int size) {
 
         Set<Integer> set = new HashSet<>();
-
         int left = 0;
         int right = 0;
         int[] teamCnt = new int[N + 1];
-
+        
+        // 투포인터로 최대 - 최소가 현재 size에 부합하면서 set.size가 N 인지 판별
         while (right < list.size()) {
+
+            int team = list.get(right).team;
+            int rightNum = list.get(right).number;
+            int leftNum = list.get(left).number;
             
-            if (set.size() < N && list.get(right).number - list.get(left).number <= size) {
-                teamCnt[list.get(right).team]++;
-                set.add(list.get(right).team);
-                if (set.size() == N && list.get(right).number - list.get(left).number <= size) {
+            // set에 팀 기록하기
+            if (set.size() < N && rightNum - leftNum <= size) {
+                teamCnt[team]++;
+                set.add(team);
+                if (set.size() == N) { // 가능한경우
                     return true;
                 }
                 right++;
                 continue;
             }
 
-            if (list.get(right).number - list.get(left).number > size) {
-                if (teamCnt[list.get(left).team] == 1) {
-                    set.remove(list.get(left).team);
-                }
-                teamCnt[list.get(left++).team]--;
+            team = list.get(left++).team; // left 팀
+            if (teamCnt[team] == 1) {
+                set.remove(team);
             }
+            teamCnt[team]--;
         }
-
         return false;
     }
 
@@ -58,17 +61,20 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        int max = 0;
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                list.add(new Node(Integer.parseInt(st.nextToken()), i));
+                int num = Integer.parseInt(st.nextToken());
+                max = Math.max(max, num);
+                list.add(new Node(num, i));
             }
         }
         Collections.sort(list);
 
-        int left = 0, right = 1_000_000_000;
+        int left = 0, right = max;
         int answer = 0;
+        // 이분탐색으로 가능한 결과값 찾기
         while (left <= right) {
             int mid = (left + right) / 2;
             if (isPossible(mid)) {
