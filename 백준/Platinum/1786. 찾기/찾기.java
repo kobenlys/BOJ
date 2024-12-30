bd9efc6e529851b98446670e074f8b79d7c42af4
영석 <149328708+kobenlys@users.noreply.github.com>
2024-12-30 20:45:47 +0900

    [Platinum V] Title: 찾기, Time: 384 ms, Memory: 50208 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -2,37 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    // 부분일치배열(LPS)만들기
     public static int[] makePattern(String str) {
         int[] pattern = new int[str.length()];
         int pos = 0;
         for (int i = 1; i < pattern.length; i++) {
+            // 불일치시 pos 조정
             while (pos > 0 && str.charAt(i) != str.charAt(pos)) {
                 pos = pattern[pos - 1];
             }
+            // 일치시 pos 증가와 LPS배열에 입력
             if (str.charAt(i) == str.charAt(pos)) {
                 pattern[i] = ++pos;
             }
         }
         return pattern;
     }
-
+    
+    // KMP 문자열 알고리즘 매칭
     public static int kmp(String str1, String str2, int[] pattern, StringBuilder sb) {
         int matchCnt = 0;
         int pos = 0;
-
         for (int i = 0; i < str1.length(); i++) {
+            // 불일치시 pos 포인트 인덱스를 LPS를 이용하여 조정
             while (pos > 0 && str1.charAt(i) != str2.charAt(pos)) {
                 pos = pattern[pos - 1];
             }
-
+            
+            // 일치시 
             if (str1.charAt(i) == str2.charAt(pos)) {
-                if (str2.length() - 1 == pos) {
-                    sb.append(i - pos + 1).append(" ");
-                    matchCnt++;
-                    pos = pattern[pos];
+                if (str2.length() - 1 == pos) { // pos가 str2길이와 같다면
+                    sb.append(i - pos + 1).append(" "); // 해당 문자열 처음위치
+                    matchCnt++; // 같은 문자열 찾은거임
+                    pos = pattern[pos];// pos값 재 조정
                 } else {
-                    pos++;
+                    pos++; // pos값을 증가시켜 다음 문자 탐색
                 }
             }
         }
