a785a51bb442593e6ccacc9e55a506a25d5be7f7
영석 <149328708+kobenlys@users.noreply.github.com>
2025-01-02 03:03:45 +0900

    [Bronze V] Title: Correct, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        N -= 9;
+
+        System.out.println(N * 60 + M);
+
+
+    }
+}
