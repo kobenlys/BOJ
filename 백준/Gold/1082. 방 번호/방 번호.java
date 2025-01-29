f2ab8dd5e6cc8d9dbc0e24c14e12a9cd98fb1ef2
영석 <149328708+kobenlys@users.noreply.github.com>
2025-01-30 04:38:30 +0900

    [Gold III] Title: 방 번호, Time: 64 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.java
+++ 백준/Gold/1082. 방 번호/방 번호.java
@@ -30,7 +30,7 @@ public class Main {
         List<Node> list = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
 
-        if (N == 1) {
+        if (N == 1) { // N == 1이면 무조건 0이 답임.
             System.out.println(0);
             return;
         }
@@ -45,9 +45,9 @@ public class Main {
         int tmpCost = 0;
         Collections.sort(list);
 
-        while (true) {
+        while (true) { // 가장 가격이 낮은 숫자로 자릿수 늘리기.
             if (totalCost < tmpCost + list.get(0).cost) break;
-            if (tmpCost == 0 && list.get(0).number == 0) {
+            if (tmpCost == 0 && list.get(0).number == 0) { // 0은 앞에 올 수 없음
                 if (totalCost < tmpCost + list.get(1).cost) break;
                 result.add(list.get(1).number);
                 tmpCost += list.get(1).cost;
@@ -57,19 +57,21 @@ public class Main {
             }
         }
 
-        Collections.sort(result, Comparator.reverseOrder());
-        if (tmpCost < totalCost) {
-            for (int i = 0; i < result.size(); i++) {
+        Collections.sort(result, Comparator.reverseOrder()); // 정렬
+        if (tmpCost < totalCost) { // 아직 사용 할 수 있는 비용이 더 남았다면
+            for (int i = 0; i < result.size(); i++) { // 가장 앞 자리부터
 
-                for (int j = N - 1; j >= 0; j--) {
+                for (int j = N - 1; j >= 0; j--) { // 큰 숫자가 들어갈 수 있는지 가격비교 진행
                     if (result.get(i) < j && totalCost >= tmpCost - arr1[result.get(i)] + arr1[j]) {
-                        tmpCost = tmpCost - arr1[result.get(i)] + arr1[j];
-                        result.set(i, j);
+                        tmpCost = tmpCost - arr1[result.get(i)] + arr1[j]; // 가격 업데이트
+                        result.set(i, j); // 숫자 업데이트
                     }
                 }
             }
         }
-
+        // 한번에 비용, 숫자를 처리할 필요가 없이 단계별로 진행
+        // 1. 자릿수 늘리기
+        // 2. 남은 비용으로 가장 앞자리 숫자 높일 수 있다면 높여보기
         for (int i = 0; i < result.size(); i++) {
             sb.append(result.get(i));
         }
