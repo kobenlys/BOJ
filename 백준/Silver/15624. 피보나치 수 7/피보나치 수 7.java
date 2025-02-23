805687216f0218f9ced79b068b00521c56aab044
영석 <149328708+kobenlys@users.noreply.github.com>
2025-02-23 15:47:44 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 216 ms, Memory: 55252 KB, Score: 200 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] dp = new int[1_000_001];
+
+    public static int dfs(int N) {
+        if (dp[N] > 0) return dp[N];
+
+        if (N == 1) {
+            return 1;
+        } else if (N == 0) {
+            return 0;
+        }
+        dp[N] = (dfs(N - 1) + dfs(N - 2)) % 1_000_000_007;
+        return dp[N];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        dfs(N);
+        System.out.println(dp[N]);
+    }
+}
