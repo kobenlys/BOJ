ffdf2795b785c51784397ec9c6cab7e890bece02
영석 <149328708+kobenlys@users.noreply.github.com>
2025-03-03 22:06:24 +0900

    [Gold I] Title: 횡단보도, Time: 1320 ms, Memory: 235776 KB -BaekjoonHub

---
+++ 백준/Gold/24042. 횡단보도/횡단보도.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static List<List<Node>> list;
+
+    public static class Node implements Comparable<Node> {
+        int goal;
+        long val;
+
+        public Node(int goal, long val) {
+            this.goal = goal;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(val, o.val);
+        }
+    }
+
+    public static long djikstra(long period, int size) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        long[] dist = new long[size + 1];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        pq.offer(new Node(1, 0));
+        dist[1] = 0;
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (dist[now.goal] < now.val) {
+                continue;
+            }
+
+            for (Node tmp : list.get(now.goal)) {
+                long nextDist;
+
+                if (now.val <= tmp.val) {
+                    nextDist = tmp.val + 1;
+                } else {
+                    nextDist = ((long) Math.ceil
+                            (((double) now.val - tmp.val) / period)) * period + tmp.val + 1;
+                }
+
+                if (dist[tmp.goal] > nextDist) {
+                    dist[tmp.goal] = nextDist;
+                    pq.offer(new Node(tmp.goal, dist[tmp.goal]));
+                }
+            }
+        }
+        return dist[size];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()); // 횡단 보도 주기
+
+        list = new ArrayList<>();
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            list.get(s).add(new Node(e, i));
+            list.get(e).add(new Node(s, i));
+        }
+
+        long answer = djikstra(M, N);
+        System.out.println(answer);
+    }
+}
