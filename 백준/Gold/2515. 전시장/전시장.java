ea0eaf1e9ed93aa0e5d994ab343afd6acae13cb6
영석 <149328708+kobenlys@users.noreply.github.com>
2025-03-04 21:57:07 +0900

    [Gold II] Title: 전시장, Time: 1120 ms, Memory: 108912 KB -BaekjoonHub

--- 백준/Gold/2515. 전시장/전시장.java
+++ 백준/Gold/2515. 전시장/전시장.java
@@ -64,11 +64,9 @@ public class Main {
         for (int i = 2; i <= N; i++) {
 
             int idx = upperBinary(1, i, arr1[i][0] - S, arr1);
-            if (idx == i + 1) {
-                dp[i][0] = arr1[i][1];
-            } else {
-                dp[i][0] = Math.max(dp[idx - 1][0], dp[idx - 1][1]) + arr1[i][1];
-            }
+           
+            dp[i][0] = Math.max(dp[idx - 1][0], dp[idx - 1][1]) + arr1[i][1];
+
             dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
             
         }
