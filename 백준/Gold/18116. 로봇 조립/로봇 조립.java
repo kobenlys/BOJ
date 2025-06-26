d3698c6b652f9daec8869c86f9caa5f4bd4c7158
영석 <149328708+kobenlys@users.noreply.github.com>
2025-06-27 02:43:26 +0900

    [Gold IV] Title: 로봇 조립, Time: 1344 ms, Memory: 374168 KB -BaekjoonHub

---
+++ 백준/Gold/18116. 로봇 조립/로봇 조립.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int[] parent;
+    public static Map<Integer, Integer> map = new HashMap<>();
+
+    public static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+
+        int from = find(x);
+        int to = find(y);
+
+        if (from != to) {
+            parent[to] = from;
+            map.put(find(x), map.get(from) + map.get(to));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        parent = new int[1_000_001];
+
+        for (int i = 1; i <= 1_000_000; i++) {
+            parent[i] = i;
+            map.put(i, 1);
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String dir = st.nextToken();
+            int A = Integer.parseInt(st.nextToken());
+
+            if (dir.equals("I")) {
+                int B = Integer.parseInt(st.nextToken());
+                union(A, B);
+            } else {
+                sb.append(map.get(find(A))).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
