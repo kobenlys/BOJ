87f9458f5f10c2334c26310bf4812d204cd84a1b
영석 <149328708+kobenlys@users.noreply.github.com>
2025-07-09 00:47:55 +0900

    [Silver V] Title: 사과 담기 게임, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()) - 1;
+
+        int J = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int nowPos = 1;
+
+        for (int i = 0; i < J; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (nowPos <= number && number <= nowPos + M) {
+                continue;
+            }
+
+            if (nowPos < number) {
+
+                for (int j = 0; j < N; j++) {
+                    nowPos++;
+                    answer++;
+                    if (nowPos <= number && number <= nowPos + M) {
+                        break;
+                    }
+                }
+
+            } else if (number < nowPos) {
+                answer += nowPos - number;
+                nowPos = number;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
