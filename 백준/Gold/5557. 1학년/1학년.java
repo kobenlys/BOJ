2499fb48f10c68d296b57e50faf5fd17b3abaeb4
영석 <149328708+kobenlys@users.noreply.github.com>
2025-09-02 22:11:54 +0900

    [Gold V] Title: 1학년, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -3,37 +3,8 @@ import java.util.*;
 
 public class Main {
 
-    public static int[] arr1;
-    public static long[][] dp;
-
     public static boolean isIn(int number) {
         return 0 <= number && number <= 20;
-    }
-
-    public static long dfs(int step, int res) {
-
-        if (dp[step][res] != -1) {
-            return dp[step][res];
-        }
-
-        if (step == arr1.length - 1) {
-            if (res == arr1[arr1.length - 1]) {
-                return dp[step][res] = 1;
-            }
-            return 0;
-        }
-
-        dp[step][res] = 0;
-
-        if (isIn(res + arr1[step])) {
-            dp[step][res] += dfs(step + 1, res + arr1[step]);
-        }
-
-        if (step > 0 && isIn(res - arr1[step])) {
-            dp[step][res] += dfs(step + 1, res - arr1[step]);
-        }
-
-        return dp[step][res];
     }
 
     public static void main(String[] args) throws IOException {
@@ -42,21 +13,28 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        arr1 = new int[N];
-        dp = new long[N][21];
-
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < 21; k++) {
-                dp[j][k] = -1;
-            }
-        }
+        int[] arr1 = new int[N];
+        long[][] dp = new long[N][21];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr1[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(0, 0);
-        System.out.println(dp[0][0]);
+        dp[0][arr1[0]] = 1;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j <= 20; j++) {
+                if (isIn(j + arr1[i])) {
+                    dp[i][j + arr1[i]] += dp[i - 1][j];
+                }
+
+                if (isIn(j - arr1[i])) {
+                    dp[i][j - arr1[i]] += dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N-2][arr1[N-1]]);
     }
 }
