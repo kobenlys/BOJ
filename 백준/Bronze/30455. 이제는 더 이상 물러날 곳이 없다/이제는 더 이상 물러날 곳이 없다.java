9382330b97dd891e72d1af1420cda62a8bd7f0c4
영석 <149328708+kobenlys@users.noreply.github.com>
2025-09-29 23:44:25 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        if(N % 2 == 0){
+            System.out.println("Duck");
+        }else{
+            System.out.println("Goose");
+        }
+        
+    }
+}
