d1dce111ae8ebdc4e33d43c21cd889f6fea3e9ea
영석 <149328708+kobenlys@users.noreply.github.com>
2025-10-03 15:54:25 +0900

    [Bronze III] Title: 어게인 포닉스, Time: 72 ms, Memory: 13072 KB -BaekjoonHub

---
+++ 백준/Bronze/33950. 어게인 포닉스/어게인 포닉스.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            String str = br.readLine();
+            sb.append(str.replaceAll("PO", "PHO")).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
