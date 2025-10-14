7b194f84efaaa850b96a67f211fa7b456fc413d5
영석 <149328708+kobenlys@users.noreply.github.com>
2025-10-14 23:32:26 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n >= a){
+            int cola = n / a * b; 
+            answer += cola;
+            n = (int) n % a;
+            n += cola;
+        }
+        
+        
+        return answer;
+    }
+}
