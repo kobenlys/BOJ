f350e2c2d67e905a1a7bd083fb55fd5d15c5baf9
영석 <149328708+kobenlys@users.noreply.github.com>
2025-10-16 00:37:37 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int L = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        N += L;
+        M += K;
+
+        if (N < M) {
+            System.out.println("Hanyang Univ.");
+        } else if (N > M) {
+            System.out.println("Yongdap");
+        }else{
+            System.out.println("Either");
+        }
+
+    }
+}
