b16f6822a477c81d17c91377ba90701a3f749126
영석 <149328708+kobenlys@users.noreply.github.com>
2025-11-17 01:45:27 +0900

    [Bronze II] Title: 스네이크, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/27512. 스네이크/스네이크.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int answer = N * M;
+
+        if (answer % 2 > 0 && N*M > 1) {
+            answer--;
+        }
+        System.out.println(answer);
+
+    }
+}
