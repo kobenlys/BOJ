360276728428158853a61e90aca35eddb44fd970
영석 <149328708+kobenlys@users.noreply.github.com>
2025-12-03 00:40:29 +0900

    [Silver II] Title: N과 M (11), Time: 492 ms, Memory: 74544 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N, M;
+    public static int[] arr1;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int depth, int[] cache) {
+
+        if (depth == M) {
+            Arrays.stream(cache).forEach(e -> sb.append(e).append(" "));
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            cache[depth] = arr1[i];
+            dfs(depth + 1, cache);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr1 = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        arr1 = Arrays.stream(arr1).distinct().sorted().toArray();
+        N = arr1.length;
+        dfs(0, new int[M]);
+        System.out.println(sb);
+    }
+}
