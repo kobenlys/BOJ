819e8afa36ee35090ce91344d64a972fd9e27d53
영석 <149328708+kobenlys@users.noreply.github.com>
2026-01-06 01:24:23 +0900

    Time: 6 ms (73.94%), Space: 67.7 MB (88.48%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -4,6 +4,7 @@ class Solution {
         long answer = 0;
         long maxMinus = Integer.MIN_VALUE;
         int minusCnt = 0;
+        int minNumber = Integer.MAX_VALUE;
         int len = matrix.length;
 
         for (int i = 0; i < len; i++) {
@@ -18,15 +19,15 @@ class Solution {
         }
 
         if (minusCnt % 2 == 0) {
+
             return answer;
         } else {
             long tmpMin = maxMinus * -1;
 
             for (int i = 0; i < len; i++) {
                 for (int j = 0; j < len; j++) {
-                    long tn = Math.abs(matrix[i][j]);
-                    if (Math.abs(matrix[i][j]) < tmpMin) {
-                        tmpMin = tn;
+                    if (Math.abs(matrix[i][j]) < maxMinus * -1) {
+                        tmpMin = Math.min(tmpMin, Math.abs(matrix[i][j]));
                     }
                 }
             }
