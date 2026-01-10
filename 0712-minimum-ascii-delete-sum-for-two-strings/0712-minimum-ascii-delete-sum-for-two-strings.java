a17a67dfb05e843f770766df49cec6a644c1e82d
영석 <149328708+kobenlys@users.noreply.github.com>
2026-01-10 14:35:00 +0900

    Time: 15 ms (91.65%), Space: 46.7 MB (27.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        
+
         int N = s1.length();
         int M = s2.length();
         int answer = 0;
-        int[][] dp = new int[N+1][M+1];
+        int[][] dp = new int[N + 1][M + 1];
 
-        for(int i = N-1; i >= 0; i--) {
-            for(int j = M-1; j >=0; j--) {
-                if(s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = s1.charAt(i) + dp[i+1][j+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = s1.charAt(i - 1) + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        for(char c : s1.toCharArray()) {
+        for (char c : s1.toCharArray()) {
             answer += c;
         }
 
-        for(char c : s2.toCharArray()) {
+        for (char c : s2.toCharArray()) {
             answer += c;
         }
-        answer -= dp[0][0] * 2;
+        answer -= dp[N][M] * 2;
         return answer;
     }
 }
